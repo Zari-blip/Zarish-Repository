41eb63a31632e4f11e5df69090a801225a242679
Zari-blip <65315@students.riphah.edu.pk>
2024-12-04 08:22:11 +0300

    Add files via upload

---
+++ lenght using cstring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+	string str="hello, world!";
+	int lenght= str.length();
+	cout<<"The lenght of the string is:"<<lenght<<endl;
+	return 0;
+}
